7d6b8b7d8a9427543e9bb204c1b79b2e5ca26123
Beaudan Brown <beaudan.brown@gmail.com>
2023-07-11 15:51:31 +1000

    Update the script to generate plots for all the possible substitutions and also compare to different cohorts (specifically those with low levels of sleep and mod/vig activity)

--- compositional_sleep_dem.R
+++ compositional_sleep_dem.R
@@ -9,12 +9,12 @@ library(dotenv)
 library(rms)
 library(survival)
 library(ggplot2)
-library(cowplot)
 
 sleep_idx <- 1
 inactive_idx <- 2
 light_idx <- 3
 vig_idx <- 4
+sub_amount <- 90
 mins_in_day <- 1440
 hours_in_day <- 24
 
@@ -71,10 +71,6 @@ full_df$vigorous_n <-
 
 full_df$mvpa_n <- full_df$moderate_n + full_df$vigorous_n
 
-comp <- acomp(data.frame(full_df$sleep_n, full_df$inactive_n, full_df$light_n, full_df$mvpa_n))
-geo_mean <-
-  apply(comp, 2, function(x) exp(mean(log(x))))
-
 sbp <- matrix(
   c(
     1, 1, -1, -1,
@@ -85,78 +81,173 @@ sbp <- matrix(
 )
 v <- gsi.buildilrBase(t(sbp))
 
+inc <- 0:sub_amount / 1440
+comp <- acomp(data.frame(full_df$sleep_n, full_df$inactive_n, full_df$light_n, full_df$mvpa_n))
 base_ilr <-
   ilr(comp, V = v) |>
   setNames(c("R1", "R2", "R3"))
-mean_ilr <-
-  ilr(acomp(geo_mean), V = v)
 
 model_data <- select(full_df, dem, time_to_dem, sex, age_assessment) |> cbind(base_ilr)
 
 options(datadist = datadist(model_data))
 model <-
   cph(
-      Surv(time_to_dem, dem) ~
-        rcs(R1, 3) + rcs(R2, 3) +
-        rcs(R3, 3) +
-        sex +
-        rcs(age_assessment, 3),
-      data = model_data
+    Surv(time_to_dem, dem) ~
+      rcs(R1, 3) + rcs(R2, 3) +
+      rcs(R3, 3) +
+      sex +
+      rcs(age_assessment, 3),
+    data = model_data
   )
 
-inc <- -90:90 / 1440
-scaled_x <- inc * 1440
+short_sleep_comp <- comp[comp$full_df.sleep_n < 0.2083]
+low_vig_comp <- comp[comp$full_df.mvpa_n < 0.03125]
+
+geo_mean <-
+  apply(comp, 2, function(x) exp(mean(log(x))))
+short_sleep_geo_mean <-
+  apply(short_sleep_comp, 2, function(x) exp(mean(log(x))))
+low_vig_geo_mean <-
+  apply(low_vig_comp, 2, function(x) exp(mean(log(x))))
+
+mean_ilr <-
+  ilr(acomp(geo_mean), V = v)
+short_sleep_mean_ilr <-
+  ilr(acomp(short_sleep_geo_mean), V = v)
+low_vig_mean_ilr <-
+  ilr(acomp(low_vig_geo_mean), V = v)
+
 base_offset <- matrix(rep(geo_mean, times = length(inc)), nrow = length(inc), byrow = TRUE)
+short_sleep_base_offset <- matrix(rep(short_sleep_geo_mean, times = length(inc)), nrow = length(inc), byrow = TRUE)
+low_vig_base_offset <- matrix(rep(low_vig_geo_mean, times = length(inc)), nrow = length(inc), byrow = TRUE)
 
 # List of replacement prefixes and their respective pairs of indexes
 # These are then used to generate and plot the relevant contrasts
-conditions <- list(sleep_inactive = c(sleep_idx, inactive_idx),
-                   sleep_light = c(sleep_idx, light_idx),
-                   sleep_vig = c(sleep_idx, vig_idx))
+sub_sleep_conditions <- list(
+  inactive_sleep = c(inactive_idx, sleep_idx),
+  light_sleep = c(light_idx, sleep_idx),
+  vig_sleep = c(vig_idx, sleep_idx),
+  sleep_inactive = c(sleep_idx, inactive_idx),
+  sleep_light = c(sleep_idx, light_idx),
+  sleep_vig = c(sleep_idx, vig_idx)
+)
 
-# Initialize your contrast list
-contrasts <- list()
+sub_inactive_conditions <- list(
+  sleep_inactive = c(sleep_idx, inactive_idx),
+  light_inactive = c(light_idx, inactive_idx),
+  vig_inactive = c(vig_idx, inactive_idx),
+  inactive_sleep = c(inactive_idx, sleep_idx),
+  inactive_light = c(inactive_idx, light_idx),
+  inactive_vig = c(inactive_idx, vig_idx)
+)
 
-# Process each condition
-for (cond in names(conditions)) {
-  # Create offset matrix
-  offset <- base_offset
-  offset[, conditions[[cond]][1]] <- base_offset[, conditions[[cond]][1]] + inc
-  offset[, conditions[[cond]][2]] <- base_offset[, conditions[[cond]][2]] - inc
+sub_light_conditions <- list(
+  inactive_light = c(inactive_idx, light_idx),
+  sleep_light = c(sleep_idx, light_idx),
+  vig_light = c(vig_idx, light_idx),
+  light_inactive = c(light_idx, inactive_idx),
+  light_sleep = c(light_idx, sleep_idx),
+  light_vig = c(light_idx, vig_idx)
+)
 
-  # Calculate ilrs
-  ilrs <- t(apply(offset, 1, function(comp) ilr(acomp(comp), V = v)))
+sub_vig_conditions <- list(
+  inactive_vig = c(inactive_idx, vig_idx),
+  light_vig = c(light_idx, vig_idx),
+  sleep_vig = c(sleep_idx, vig_idx),
+  vig_inactive = c(vig_idx, inactive_idx),
+  vig_light = c(vig_idx, light_idx),
+  vig_sleep = c(vig_idx, sleep_idx)
+)
 
-  # Get contrasts
-  contrasts[[cond]] <- apply(ilrs, 1, function(new_ilr) {
-    contrast_out <- contrast(
-      model,
-      list(R1 = new_ilr[1], R2 = new_ilr[2], R3 = new_ilr[3]),
-      list(R1 = mean_ilr[1], R2 = mean_ilr[2], R3 = mean_ilr[3])
+generate_plot <- function(conditions, base_offset, mean_ilr, comp) {
+  # Initialize your contrast list
+  contrasts <- list()
+
+  # Process each condition
+  for (cond in names(conditions)) {
+    # Create offset matrix
+    offset <- base_offset
+    offset[, conditions[[cond]][1]] <- base_offset[, conditions[[cond]][1]] + inc
+    offset[, conditions[[cond]][2]] <- base_offset[, conditions[[cond]][2]] - inc
+
+    # Calculate ilrs
+    ilrs <- t(apply(offset, 1, function(comp) ilr(acomp(comp), V = v)))
+
+    # Get contrasts
+    contrasts[[cond]] <- apply(ilrs, 1, function(new_ilr) {
+      contrast_out <- contrast(
+        model,
+        list(R1 = new_ilr[1], R2 = new_ilr[2], R3 = new_ilr[3]),
+        list(R1 = mean_ilr[1], R2 = mean_ilr[2], R3 = mean_ilr[3])
+      )
+      return(c(contrast_out$Contrast, contrast_out$Lower, contrast_out$Upper))
+    })
+  }
+
+  # Create an empty data frame
+  df <- data.frame()
+
+  scaled_x <- inc * 1440
+  # Iterate over conditions and add data to the dataframe
+  for (cond in names(conditions)) {
+    temp_df <- data.frame(
+      X = scaled_x,
+      Y = contrasts[[cond]][1, ],
+      Y_lower = contrasts[[cond]][2, ],
+      Y_upper = contrasts[[cond]][3, ],
+      condition = cond
     )
-    return(c(contrast_out$Contrast, contrast_out$Lower, contrast_out$Upper))
-  })
+    df <- rbind(df, temp_df)
+  }
+
+  return(ggplot(df, aes(x = X)) +
+    geom_line(aes(y = Y, color = condition)) +
+    geom_ribbon(aes(ymin = Y_lower, ymax = Y_upper, fill = condition), alpha = 0.2) +
+    xlab("min/day realocated") +
+    ylab("Delta log hazard") +
+    geom_hline(yintercept = 0))
 }
 
-# Create an empty data frame
-df <- data.frame()
+sub_sleep_base_plot <-
+  generate_plot(sub_sleep_conditions, base_offset, mean_ilr, comp)
+short_sleep_sub_sleep_plot <-
+  generate_plot(sub_sleep_conditions, short_sleep_base_offset, short_sleep_mean_ilr, comp)
+low_vig_sub_sleep_plot <-
+  generate_plot(sub_sleep_conditions, low_vig_base_offset, low_vig_mean_ilr, comp)
 
-# Iterate over conditions and add data to the dataframe
-for (cond in names(conditions)) {
-  temp_df <- data.frame(
-    X = scaled_x,
-    Y = contrasts[[cond]][1, ],
-    Y_lower = contrasts[[cond]][2, ],
-    Y_upper = contrasts[[cond]][3, ],
-    condition = cond
-  )
-  df <- rbind(df, temp_df)
-}
+sub_inactive_base_plot <-
+  generate_plot(sub_inactive_conditions, base_offset, mean_ilr, comp)
+short_sleep_sub_inactive_plot <-
+  generate_plot(sub_inactive_conditions, short_sleep_base_offset, short_sleep_mean_ilr, comp)
+low_vig_sub_inactive_plot <-
+  generate_plot(sub_inactive_conditions, low_vig_base_offset, low_vig_mean_ilr, comp)
 
-# Plot using ggplot
-ggplot(df, aes(x = X)) +
-  geom_line(aes(y = Y, color = condition)) +
-  geom_ribbon(aes(ymin = Y_lower, ymax = Y_upper, fill = condition), alpha = 0.2) +
-  xlab("min/day realocated") +
-  ylab("Delta log hazard") +
-  theme_cowplot()
+sub_light_base_plot <-
+  generate_plot(sub_light_conditions, base_offset, mean_ilr, comp)
+short_sleep_sub_light_plot <-
+  generate_plot(sub_light_conditions, short_sleep_base_offset, short_sleep_mean_ilr, comp)
+low_vig_sub_light_plot <-
+  generate_plot(sub_light_conditions, low_vig_base_offset, low_vig_mean_ilr, comp)
+
+sub_vig_base_plot <-
+  generate_plot(sub_vig_conditions, base_offset, mean_ilr, comp)
+short_sleep_sub_vig_plot <-
+  generate_plot(sub_vig_conditions, short_sleep_base_offset, short_sleep_mean_ilr, comp)
+low_vig_sub_vig_plot <-
+  generate_plot(sub_vig_conditions, low_vig_base_offset, low_vig_mean_ilr, comp)
+
+ggsave(filename = "plots/sub_sleep_base_plot.png", plot = sub_sleep_base_plot)
+ggsave(filename = "plots/short_sleep_sub_sleep_plot.png", plot = short_sleep_sub_sleep_plot)
+ggsave(filename = "plots/low_vig_sub_sleep.png", plot = low_vig_sub_sleep_plot)
+
+ggsave(filename = "plots/sub_inactive_base_plot.png", plot = sub_inactive_base_plot)
+ggsave(filename = "plots/short_sleep_sub_inactive_plot.png", plot = short_sleep_sub_inactive_plot)
+ggsave(filename = "plots/low_vig_sub_inactive.png", plot = low_vig_sub_inactive_plot)
+
+ggsave(filename = "plots/sub_light_base_plot.png", plot = sub_light_base_plot)
+ggsave(filename = "plots/short_sleep_sub_light_plot.png", plot = short_sleep_sub_light_plot)
+ggsave(filename = "plots/low_vig_sub_light.png", plot = low_vig_sub_light_plot)
+
+ggsave(filename = "plots/sub_vig_base_plot.png", plot = sub_vig_base_plot)
+ggsave(filename = "plots/short_sleep_sub_vig_plot.png", plot = short_sleep_sub_vig_plot)
+ggsave(filename = "plots/low_vig_sub_vig.png", plot = low_vig_sub_vig_plot)

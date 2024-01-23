536aaf9f12c6ecef8175195f7ac7bfade42efaae
Lachlan-Cribb <Lachycribb@gmail.com>
2024-01-23 17:29:35 +1100

    new sensitivity analysis

--- utils.R
+++ utils.R
@@ -25,3 +25,29 @@ sbp <- matrix(
 )
 
 v <- gsi.buildilrBase(t(sbp))
+
+
+## strip unneccessary model components  
+
+strip_glm <- function(cm) {
+  cm$y <- c()
+  cm$model <- c()
+  
+  cm$residuals <- c()
+  cm$fitted.values <- c()
+  cm$effects <- c()
+  cm$qr$qr <- c()
+  cm$linear.predictors <- c()
+  cm$weights <- c()
+  cm$prior.weights <- c()
+  cm$data <- c()
+  
+  cm$family$variance <- c()
+  cm$family$dev.resids <- c()
+  cm$family$aic <- c()
+  cm$family$validmu <- c()
+  cm$family$simulate <- c()
+  
+  return(cm)
+}
+

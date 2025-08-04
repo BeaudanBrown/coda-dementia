add949e3bb7bbe8a02ca7f7b3eba9d623785506d
Beaudan Brown <beaudan.brown@gmail.com>
2025-08-04 13:52:28 +1000

    Add old cum

--- cum_targets.R
+++ cum_targets.R
@@ -48,6 +48,159 @@ cum_targets <- list(
       )
     }
   ),
+  tar_target(
+    old_reference_comps,
+    {
+      best_vars <- data.table(
+        avg_sleep = 7.25,
+        avg_inactivity = 11.5,
+        avg_light = 2,
+        avg_mvpa = 3.25
+      ) *
+        60
+      old_best <- acomp(best_vars)
+
+      typical_vars <- data.table(
+        avg_sleep = 6.5,
+        avg_inactivity = 12,
+        avg_light = 3.5,
+        avg_mvpa = 2
+      ) *
+        60
+      old_typical <- acomp(typical_vars)
+      worst_vars <- data.table(
+        avg_sleep = 4.5,
+        avg_inactivity = 15.25,
+        avg_light = 3.75,
+        avg_mvpa = 0.5
+      ) *
+        60
+      old_worst <- acomp(worst_vars)
+
+      best_ilr <-
+        ilr(old_best, V = v) |>
+        setNames(c("R1", "R2", "R3")) |>
+        t() |>
+        as.data.table()
+
+      typical_ilr <-
+        ilr(old_typical, V = v) |>
+        setNames(c("R1", "R2", "R3")) |>
+        t() |>
+        as.data.table()
+
+      worst_ilr <-
+        ilr(old_worst, V = v) |>
+        setNames(c("R1", "R2", "R3")) |>
+        t() |>
+        as.data.table()
+
+      best_comp <- cbind(best_vars, best_ilr)
+      worst_comp <- cbind(worst_vars, worst_ilr)
+      typical_comp <- cbind(typical_vars, typical_ilr)
+
+      list(
+        Best = best_comp,
+        Worst = worst_comp,
+        Typical = typical_comp
+      )
+    }
+  ),
+
+  tar_map(
+    values = list(
+      comp = c(
+        "Best",
+        "Typical",
+        "Worst"
+      )
+    ),
+    names = comp,
+    tar_target(
+      old_cum_risks,
+      {
+        imp_test[, c("R1", "R2", "R3")] <-
+          old_reference_comps[[comp]][, c("R1", "R2", "R3")]
+        risks <- get_risk(imp_test, test_models, final_time)
+        risks <- risks[, .(eid, risk, timegroup)]
+        risks[,
+          .(
+            risk = mean(risk, na.rm = TRUE),
+            B = unique(imp_test$tar_batch)
+          ),
+          by = .(timegroup)
+        ]
+      },
+      pattern = map(imp_test, test_models)
+    ),
+    tar_target(
+      old_cum_avg_risks,
+      {
+        old_cum_risks[,
+          .(
+            risk = mean(risk),
+            lower_risk = quantile(risk, 0.025),
+            upper_risk = quantile(risk, 0.975),
+            Composition = comp
+          ),
+          by = timegroup
+        ]
+      }
+    )
+  ),
+
+  tar_target(
+    old_cum_plot_data,
+    {
+      bind_rows(
+        cum_avg_risks_Worst,
+        cum_avg_risks_Typical,
+        cum_avg_risks_Best
+      )
+    }
+  ),
+  tar_target(
+    old_cum_plot,
+    {
+      composition_colors <- c(
+        "Worst" = "#ff747b",
+        "Typical" = "#708ff9",
+        "Best" = "#6ed853"
+      )
+      old_cum_plot_data |>
+        ggplot(aes(x = timegroup, y = risk)) +
+        geom_ribbon(
+          aes(ymin = lower_risk, ymax = upper_risk, fill = Composition),
+          alpha = 0.25
+        ) +
+        geom_line(aes(colour = Composition)) +
+        labs(
+          x = "Time since baseline (years)",
+          y = "Cumulative all-cause dementia incidence"
+        ) +
+        scale_color_manual(
+          values = composition_colors
+        ) +
+        scale_fill_manual(
+          values = composition_colors
+        ) +
+        cowplot::theme_cowplot(
+          font_size = 16,
+          font_family = "serif",
+          line_size = 0.25
+        ) +
+        theme(
+          panel.border = element_rect(fill = NA, colour = "#585656"),
+          panel.grid = element_line(colour = "grey92"),
+          panel.grid.minor = element_line(linewidth = rel(0.5)),
+          axis.ticks.y = element_blank(),
+          axis.line = element_line(color = "#585656"),
+          axis.title.x = element_text(family = "serif", size = 20),
+          axis.title.y = element_text(family = "serif", size = 20)
+        )
+    }
+  ),
+
   tar_map(
     values = list(
       comp = c(
